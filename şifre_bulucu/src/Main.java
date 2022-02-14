2865072429619b8249879a7724c90fa4bf71316e
Caduseus4 <ayitkisakir1@gmail.com>
2022-02-14 21:22:43 +0300

    şifre bulucu
    şifre bulucu

---
+++ şifre_bulucu/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int pin=5789;
+
+        for (int i=0; i<=9999;i++){
+
+
+            if(i==pin){
+                System.out.println("****************************************");
+                System.out.println("ŞİFRE TESPİT EDİLDİ!");
+                System.out.println(i);
+                System.out.println("****************************************");
+break;
+            }else{
+
+            }
+
+        }
+
+
+
+    }
+}
